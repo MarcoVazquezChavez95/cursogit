6ed92f713453d367ecb5be6ea70e441ed14e34cd
MarcoVazquezChavez95 <marcovazquezchavez95@gmail.com>
2022-11-23 21:49:53 +0000

    Se añadieron 3 archivos nuevos

---
+++ validar.php
@@ -0,0 +1,13 @@
+<html>      
+<head>       
+<title>Javascript Números aleatorios</title>       
+</head>       
+<body>       
+<script language=Javascript>       
+<!--
+  var rand_NumeroAleatorio = Math.floor((999-4)*Math.random()) + 1;   
+  -->      
+    </script>       
+<form> <input type="button" name="Generaaleatorio" value="Genera Numero Aleatorio" onclick="alert(rand_NumeroAleatorio);">      
+</form>      
+</body></html>
